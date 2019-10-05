421101da0af84ed255b97d006a695132d69e0acb
MaiThiQuanh <43835870+MaiThiQuanh@users.noreply.github.com>
2019-10-05 21:00:11 +0700

    fourth commit

---
+++ MaiThiQuanh_EXE/Vidu1ES6.js
@@ -0,0 +1,22 @@
+
+//Tính tổng, hiệu, tích, thương hai số
+//Khai báo
+var num1=10
+var num2=2
+var res=0
+
+//Tính tổng hai số
+res= num1+num2
+console.log("Tổng hai số: "+ res);
+
+//Tính hiệu hai số
+res=num1-num2;
+console.log("Hiệu hai số: "+res)
+
+//Tính tích hai số
+res=num1*num2
+console.log("Tích hai số: "+res)
+
+//Tính thương hai số
+res=num1/num2
+console.log("Thương hai số: "+res)
